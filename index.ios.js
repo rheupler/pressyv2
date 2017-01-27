edac259400a5970b4e2effd4dfe2aa23378a61b5
Ryan <rheupler@gmail.com>
2017-01-26 19:46:35 -0500

    adds basic template for start project

---
+++ index.ios.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class colored extends Component {
+  render() {
+    return (
+      <View style={styles.cirlces}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  circles: {
+    borderRadius: '50%',
+    width: '200px',
+    height: '200px',
+  }
+});
+
+AppRegistry.registerComponent('colored', () => colored);
